b2ebeb793de2228213a8f4270723427d6da8e36d
Zelun Kong <zelun.kong@outlook.com>
2018-03-30 20:12:43 -0500

    2012

--- ddml.py
+++ ddml.py
@@ -311,9 +311,9 @@ class Net(nn.Module):
 
 if __name__ == '__main__':
 
-    train_epoch_number = 1000
+    train_epoch_number = 5000
     train_batch_size = 10
-    test_data_size = 100
+    test_data_size = 10000
 
     layer_shape = (784, 392, 196, 98)
 
