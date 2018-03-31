4d918aaf61917fe40f52db873cc85722c414240e
Zelun Kong <zelun.kong@outlook.com>
2018-03-30 19:20:48 -0500

    1921

--- ddml_batch_2.py
+++ ddml_batch_2.py
@@ -186,7 +186,7 @@ class Net(nn.Module):
         partial_derivative_W_m = [self.lambda_ * params_M[m] for m in range(self.layer_count - 1)]
 
         for m in range(self.layer_count - 1):
-            for i in range(len(dataloader))
+            for i in range(len(dataloader)):
                 for j in range(len(dataloader)):
                     partial_derivative_W_m[m] += (delta_ij_m[i][j][m] * h_i_m[i][m].t()).t() + (delta_ij_m[j][i][m] * h_i_m[j][m].t()).t()
 
