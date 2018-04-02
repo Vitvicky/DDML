455d2f50d36280b71cbd1ec3f57c16879c902844
Zelun Kong <zelun.kong@outlook.com>
2018-04-02 16:32:16 -0500

    2018-0402-1632

--- ddml.py
+++ ddml.py
@@ -2,7 +2,6 @@ import os
 import random
 import logging
 import math
-import numpy as np
 import torch
 import torch.cuda as cuda
 from torch import FloatTensor
@@ -35,22 +34,39 @@ class DDMLDataset(Dataset):
 
     file_path = 'fashion-mnist_test.csv'
     dataset = []
+    labels = set()
     with open(file_path) as f:
         for line in f:
             row = [float(_) for _ in line.split(',')]
             dataset.append((row[:-1], row[-1:]))
+            labels.add(int(row[-1]))
 
-    def __init__(self, size=10):
+    def __init__(self, label, size=10):
+        """
+
+        :param label: the label must in the pairs.
+        :param size: size of the dataloader.
+        """
         self.data = []
 
-        if cuda.is_available():
-            tensor = cuda.FloatTensor
-        else:
-            tensor = FloatTensor
+        if label not in DDMLDataset.labels:
+            raise ValueError("Label not in the dataset.")
+
+        self.label = label
+
+        # if cuda.is_available():
+        #     tensor = cuda.FloatTensor
+        # else:
+        #     tensor = FloatTensor
+
+        tensor = FloatTensor
 
         while len(self.data) < size:
-            s1 = random.choice(DDMLDataset.dataset)
-            s2 = random.choice(DDMLDataset.dataset)
+            while True:
+                s1 = random.choice(DDMLDataset.dataset)
+                s2 = random.choice(DDMLDataset.dataset)
+                if int(s1[1][0]) == self.label:
+                    break
 
             self.data.append(((tensor(s1[0]) / 255, tensor(s1[1])), (tensor(s2[0]) / 255, tensor(s2[1]))))
 
@@ -64,7 +80,7 @@ class DDMLDataset(Dataset):
 
 class Net(nn.Module):
 
-    def __init__(self, layer_shape, beta=0.5, tao=5, lambda_=0.01, learning_rate=0.001):
+    def __init__(self, layer_shape, beta=0.5, tao=5.0, lambda_=0.01, learning_rate=0.01):
         """
 
         :param layer_shape:
@@ -95,23 +111,13 @@ class Net(nn.Module):
         self.gradient = []
         self.logger = logging.getLogger(__name__)
 
-    def _compute_distance(self, feature1, feature2):
-        """
-        Compute the distance of two samples.
-        ------------------------------------
-        :param feature1: Variable
-        :param feature2: Variable
-        :return: The distance of the two sample.
-        """
-        return (self(feature1) - self(feature2)).data.norm() ** 2
-
     def _g(self, z):
         """
         Generalized logistic loss function.
         -----------------------------------
         :param z:
         """
-        return float((np.log(1 + np.exp(self.beta * z))) / self.beta)
+        return float(math.log(1 + math.exp(self.beta * z)) / self.beta)
 
     def _g_derivative(self, z):
         """
@@ -119,7 +125,7 @@ class Net(nn.Module):
         -----------------------
         :param z:
         """
-        return float(1 / (np.exp(-1 * self.beta * z) + 1))
+        return float(1 / (math.exp(-1 * self.beta * z) + 1))
 
     def _s_derivative(self, z):
         """
@@ -206,7 +212,7 @@ class Net(nn.Module):
             else:
                 l = -1
 
-            c = 1 - l * (self.tao - self._compute_distance(xi, xj))
+            c = 1 - l * (self.tao - self.compute_distance(xi, xj))
 
             # h(m) have M + 1 values and m start from 0, in fact, delta_ij_m have M value and m start from 1
             delta_ij_m[index][M] = (self._g_derivative(c) * l * (h_i_m[index][M + 1] - h_j_m[index][M + 1])) * self._s_derivative(z_i_m[index][M])
@@ -271,8 +277,8 @@ class Net(nn.Module):
 
         # J1
         for si, sj in dataloader:
-            xi = Variable(si[0])
-            xj = Variable(sj[0])
+            xi = Variable(si[0], requires_grad = True)
+            xj = Variable(sj[0], requires_grad = True)
             yi = si[1]
             yj = sj[1]
 
@@ -281,7 +287,8 @@ class Net(nn.Module):
             else:
                 l = -1
 
-            dist = self._compute_distance(xi, xj)
+            dist = self.compute_distance(xi, xj)
+            # dist = (self(xi) - self(xj)).data.norm()
             c = 1 - l * (self.tao - dist)
             loss1 += self._g(c)
 
@@ -299,55 +306,57 @@ class Net(nn.Module):
 
         return loss1, loss2
 
-    def is_similar(self, feature1, feature2):
+    def compute_distance(self, feature1, feature2):
         """
-        Determine is two sample is from the same project.
-        ---------------------------------------------------
-        :param feature1: Variable, the feature of sample1
-        :param feature2: Variable, the feature of sample2
-        :return: the result and the distance of the two sample.
+        Compute the distance of two samples.
+        ------------------------------------
+        :param feature1: Variable
+        :param feature2: Variable
+        :return: The distance of the two sample.
         """
-        distance = self._compute_distance(feature1, feature2)
-        result = distance <= self.tao * 1.5
-
-        return result, distance
+        return (self(feature1) - self(feature2)).data.norm() ** 2
 
 
-if __name__ == '__main__':
+def main():
+    test_label = 0
 
-    train_epoch_number = 10000
+    train_epoch_number = 1000
     train_batch_size = 10
     test_data_size = 10000
 
-    layer_shape = (784, 392, 196)
+    layer_shape = (784, 392, 196, 98)
 
     # logger = setup_logger()
     logger = setup_logger(level=logging.INFO)
 
-    test_data = DDMLDataset(size=test_data_size)
+    test_data = DDMLDataset(label=test_label, size=test_data_size)
     test_data_loader = DataLoader(dataset=test_data)
 
-    net = Net(layer_shape, beta=0.5, tao=5, lambda_=0.01, learning_rate=0.001)
+    net = Net(layer_shape, beta=5.0, tao=3.0, lambda_=0.001, learning_rate=0.01)
 
-    pkl = "pkl/ddml({} {} {} {}).pkl".format(net.beta, net.tao, net.lambda_, net.learning_rate)
-
-    if cuda.is_available():
-        net.cuda()
-        logger.info("Using cuda!")
+    pkl = "pkl/ddml({}: {}-{}-{}).pkl".format(test_label, net.beta, net.tao, net.lambda_)
+    # if cuda.is_available():
+    #     net.cuda()
+    #     logger.info("Using cuda!")
 
     if os.path.exists(pkl):
         state_dict = torch.load(pkl)
         net.load_state_dict(state_dict)
-    else:
-        for epoch in range(train_epoch_number):
-            train_data = DDMLDataset(size=train_batch_size)
-            train_data_loader = DataLoader(dataset=train_data)
-            net.compute_gradient(train_data_loader)
-            net.backward()
-            loss1, loss2 = net.compute_loss(train_data_loader)
-            logger.info("Iteration: %6d, Loss1: %9.3f, Loss2: %9.3f", epoch + 1, loss1, loss2)
+        logger.info("Load state from file.")
 
-            torch.save(net.state_dict(), "pkl/ddml({} {} {} {}).pkl".format(net.beta, net.tao, net.lambda_, net.learning_rate))
+    for epoch in range(train_epoch_number):
+        train_data = DDMLDataset(label=test_label, size=train_batch_size)
+        train_data_loader = DataLoader(dataset=train_data)
+        net.compute_gradient(train_data_loader)
+        net.backward()
+        loss1, loss2 = net.compute_loss(train_data_loader)
+        logger.info("Iteration: %6d, Loss1: %6.3f, Loss2: %6.3f", epoch + 1, loss1, loss2)
+
+    torch.save(net.state_dict(), pkl)
+
+    #
+    # test
+    #
 
     similar_dist_sum = 0.0
     dissimilar_dist_sum = 0.0
@@ -357,14 +366,21 @@ if __name__ == '__main__':
     dissimilar_correct = 0
     num = 0
 
+    distance_list = [0 for l in DDMLDataset.labels]
+    pairs_count = [0 for l in DDMLDataset.labels]
+
     for si, sj in test_data_loader:
         xi = Variable(si[0])
-        yi = si[1]
+        yi = int(si[1])
         xj = Variable(sj[0])
-        yj = sj[1]
+        yj = int(sj[1])
 
-        actual = (int(yi) == int(yj))
-        result, dist = net.is_similar(xi, xj)
+        actual = (yi == yj)
+        dist = net.compute_distance(xi, xj)
+        result = (dist <= net.tao + 1)
+
+        distance_list[yj] += dist
+        pairs_count[yj] += 1
 
         if actual:
             similar_dist_sum += dist
@@ -381,8 +397,49 @@ if __name__ == '__main__':
 
         num += 1
 
-        logger.info("%6d, %5s, %5s, %9.3f", num, actual, result, dist)
+        logger.info("%6d, %2d, %2d, %9.3f", num, int(yi), int(yj), dist)
 
-    logger.info("Similar Average Distance: %.6f", similar_dist_sum / (similar_correct + similar_incorrect))
+    logger.info("Similar: Average Distance: %.6f", similar_dist_sum / (similar_correct + similar_incorrect))
     logger.info("Dissimilar: Average Distance: %.6f", dissimilar_dist_sum / (dissimilar_correct + dissimilar_incorrect))
     logger.info("\nConfusion Matrix:\n\t%6d\t%6d\n\t%6d\t%6d", similar_correct, similar_incorrect, dissimilar_incorrect, dissimilar_correct)
+
+    print('   ', end='')
+    for label in DDMLDataset.labels:
+        print('{:^7}'.format(label), end='\t')
+    print('\n{}: '.format(test_label), end='')
+
+    for l in DDMLDataset.labels:
+        try:
+            v = '{:.3f}'.format(distance_list[l] / pairs_count[l])
+        except ZeroDivisionError:
+            v = ' None '
+
+        print(v, end='\t')
+
+
+if __name__ == '__main__':
+    main()
+
+# def main_old():
+# distance_matrix = [[0 for l2 in DDMLDataset.labels] for l1 in DDMLDataset.labels]
+# pairs_count = [[0 for l2 in DDMLDataset.labels] for l1 in DDMLDataset.labels]
+#
+# for si, sj in test_data_loader:
+#     distance_matrix[l1][l2] += dist
+#     pairs_count[l1][l2] += 1
+
+# print('   ', end='')
+# for label in DDMLDataset.labels:
+#     print('{:^7}'.format(label), end='\t')
+#
+# print()
+#
+# for l1 in DDMLDataset.labels:
+#     print(l1, end=': ')
+#     for l2 in DDMLDataset.labels:
+#         try:
+#             v = '{:.3f}'.format(distance_matrix[l1][l2] / pairs_count[l1][l2])
+#         except ZeroDivisionError:
+#             v = ' None '
+#         print(v, end='\t')
+#     print()
